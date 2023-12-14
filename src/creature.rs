671836c4d789a6aa930a67352ecaf9aa7344a579
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-12-14 14:08:32 +0800

    Added Creature struct

--- src/creature.rs
+++ src/creature.rs
@@ -1,1 +1,13 @@
+use genome::Genome;
+use neuron::{Connection, InputNeuron, ActionNeuron, InternalNeuron};
+use vector2d::Vector2D;
 
+
+struct Creature {
+    genome: Genome,
+    position: Vector2D<usize>,
+    connections: Vec<Connection>,
+    input_neurons: Vec<Box<InputNeuron>>,
+    internal_neurons: Vec<Box<InternalNeuron>>,
+    action_neurons: Vec<Box<ActionNeuron>>
+}
