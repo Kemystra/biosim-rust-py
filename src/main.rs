a5221319736417b7491325eba8f5418c97a4fae3
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-12-14 11:48:00 +0800

    Fuck You

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,55 @@
 use std::num::NonZeroU32;
-
-use winit::window::WindowBuilder;
+use std::rc::Rc;
+use softbuffer::Buffer;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-
-use softbuffer::{Context, Surface};
-
-struct Renderer {
-    width: usize,
-    height: usize,
-    tmp_buffer: Vec<u32>
-}
-
-impl Renderer {
-    fn new(width: usize, height: usize) -> Self {
-        Self {
-            width,
-            height,
-            tmp_buffer: vec![0; width*height]
-        }
-    }
-
-    fn plot_pixel(&mut self, x: usize, y: usize, color: u32) {
-        self.tmp_buffer[x + (y*self.width)] = color;
-    }
-
-    fn render(&mut self) -> &Vec<u32> {
-        let color = 255 | (255 << 8) | (255 << 16);
-
-        for y in 0..400 {
-            for x in 0..400 {
-                self.plot_pixel(x,y,color);
-            }
-        }
-
-        &self.tmp_buffer
-    }
-}
+use winit::window::WindowBuilder;
 
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    
-    let context = unsafe { Context::new(&window) }.unwrap();
-    let mut surface = unsafe { Surface::new(&context, &window) }.unwrap();
+    let event_loop = EventLoop::new().unwrap();
+    let window = Rc::new(WindowBuilder::new().build(&event_loop).unwrap());
+    let context = softbuffer::Context::new(window.clone()).unwrap();
+    let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
 
-    let (width, height) = { let size = window.inner_size(); (size.width, size.height) };
-    let mut renderer = Renderer::new(width as usize, height as usize);
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    event_loop.run(move |event, elwt| {
+        elwt.set_control_flow(ControlFlow::Wait);
 
         match event {
-            Event::WindowEvent { window_id, event: WindowEvent::CloseRequested }
-                if window_id == window.id() => { *control_flow = ControlFlow::Exit }
-
-            // Exit window with ANY keypress
-            Event::WindowEvent { window_id, event: WindowEvent::KeyboardInput {..} }
-                if window_id == window.id() => { *control_flow = ControlFlow::Exit }
-
-            Event::RedrawRequested(window_id)
-                if window_id == window.id() => {
-                    let (width, height) = { let size = window.inner_size(); (size.width, size.height) };
-                    surface.resize(
+            Event::WindowEvent { window_id, event: WindowEvent::RedrawRequested } if window_id == window.id() => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
                         NonZeroU32::new(width).unwrap(),
-                        NonZeroU32::new(height).unwrap()
-                    ).unwrap();
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
 
-                    let mut buffer = surface.buffer_mut().unwrap();
-
-                    buffer.copy_from_slice(renderer.render());
-
-                    buffer.present().unwrap();
-                }
-
+                let mut buffer = surface.buffer_mut().unwrap();
+                color_buffer(&mut buffer, width, height);
+                buffer.present().unwrap();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
             _ => {}
         }
-    });
+    }).unwrap();
+}
+
+fn color_buffer(buffer: &mut [u32], width: u32, height: u32) {
+    for index in 0..(width * height) {
+        let y = index / width;
+        let x = index % width;
+        let red = x % 255;
+        let green = y % 255;
+        let blue = (x * y) % 255;
+
+        buffer[index as usize] = blue | (green << 8) | (red << 16);
+    }
 }
