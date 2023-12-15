c20d03d0be7805e78f0d546991fb5c04f7ac5918
Izzminhal Akmal bin Norhisyam <izzmin97@gmail.com>
2023-12-15 15:55:33 +0800

    Added SimRenderer to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::rc::Rc;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit::dpi::PhysicalSize;
 
 mod simulation;
 mod creature;
@@ -14,25 +15,33 @@ mod vector2d;
 use simulation::Simulation;
 use renderer::SimRenderer;
 
+const VIEWPORT_WIDTH: u32 = 500;
+const VIEWPORT_HEIGHT: u32 = 500;
+
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let window = Rc::new(WindowBuilder::new().build(&event_loop).unwrap());
+
+    let window_size = PhysicalSize::new(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    let window = Rc::new(WindowBuilder::new()
+        .with_inner_size(window_size)
+        .with_title("Neural Network, No Cap")
+        .build(&event_loop).unwrap());
+
     let context = softbuffer::Context::new(window.clone()).unwrap();
     let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+
+    let renderer = SimRenderer::new(VIEWPORT_WIDTH as usize, VIEWPORT_HEIGHT as usize);
 
     event_loop.run(move |event, elwt| {
         elwt.set_control_flow(ControlFlow::Wait);
 
         match event {
             Event::WindowEvent { window_id, event: WindowEvent::RedrawRequested } if window_id == window.id() => {
-                let (width, height) = {
-                    let size = window.inner_size();
-                    (size.width, size.height)
-                };
                 surface
                     .resize(
-                        NonZeroU32::new(width).unwrap(),
-                        NonZeroU32::new(height).unwrap(),
+                        NonZeroU32::new(VIEWPORT_WIDTH).unwrap(),
+                        NonZeroU32::new(VIEWPORT_HEIGHT).unwrap(),
                     )
                     .unwrap();
 
