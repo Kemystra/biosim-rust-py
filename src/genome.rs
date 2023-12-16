fa3a46fa883f3cee2f73bc12989c00b2da1ea0b0
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-12-16 17:53:37 +0800

    Added new() to Genome

--- src/genome.rs
+++ src/genome.rs
@@ -7,10 +7,25 @@ use crate::neuron::Brain;
 pub struct Genome(Vec<u16>);
 
 impl Genome {
+    // Combine 2 bytes, and collect
+    pub fn new(bytes: &[u8]) -> Self {
+        let result = vec![];
+        let n = 0;
+        let gene: u16;
+
+        for i in 0..(bytes.len() / 2) {
+            n = i*2;
+            gene = (bytes[n] as u16) | (bytes[n+1] as u16) << 8;
+            result.push(gene);
+        }
+
+        Genome(result)
+    }
+
     pub fn generate_color(&self) -> Color {
 
     }
-
+/*
     pub fn generate_brain(&self) -> Brain {
 
     }
@@ -18,7 +33,7 @@ impl Genome {
     pub fn replicate<R: SeedableRng>(&mut self, rng: R) -> Self {
 
     }
-
+*/
     fn randomly_mutate<R: SeedableRng>(&mut self, rng: R) {
 
     }
