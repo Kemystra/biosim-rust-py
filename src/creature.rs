e96328f7093c1c6b3b2b6ef70e629f0fe6230ec2
Kemystra <74447600+Kemystra@users.noreply.github.com>
2023-12-17 14:51:15 +0800

    Added color property to Creature and setup getters for position and color

--- src/creature.rs
+++ src/creature.rs
@@ -1,4 +1,5 @@
 use crate::genome::Genome;
+use crate::renderer::Color;
 
 use crate::neuron::{Connection, input_neuron, action_neuron, internal_neuron};
 use input_neuron::InputNeuron;
@@ -9,10 +10,23 @@ use crate::vector2d::Vector2D;
 
 
 pub struct Creature {
-    genome: Genome,
     position: Vector2D<usize>,
+
+    genome: Genome,
+    color: Color,
+
     connections: Vec<Connection>,
     input_neurons: Vec<Box<dyn InputNeuron>>,
     internal_neurons: Vec<Box<InternalNeuron>>,
     action_neurons: Vec<Box<dyn ActionNeuron>>
+}
+
+impl Creature {
+    pub fn position(&self) -> Vector2D<usize> {
+        self.position
+    }
+
+    pub fn color(&self) -> Color {
+        self.color
+    }
 }
