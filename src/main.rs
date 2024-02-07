72a4abc2274892f16624488c0604496b882e96bf
Kemystra <programmer.coleslaw981@slmail.me>
2024-02-07 16:14:13 +0800

    Scraped off real-time rendering to just outputting image files lul

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use std::num::NonZeroU32;
-use std::rc::Rc;
-use std::time::{Instant, Duration};
-
-use winit::event::{Event, WindowEvent, KeyEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
-use winit::dpi::PhysicalSize;
-use winit::keyboard::{Key, NamedKey};
-
 mod simulation;
 mod creature;
 mod genome;
@@ -23,94 +13,15 @@ const FIELD_HEIGHT: usize = 100;
 
 const BLOCK_SIZE: usize = 5;
 
-const MICROSECONDS_PER_FRAME: u64 = 1_000_000 / 60;
-
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let viewport_width = (FIELD_WIDTH * BLOCK_SIZE + BLOCK_SIZE) as u32;
-    let viewport_height = (FIELD_HEIGHT * BLOCK_SIZE + BLOCK_SIZE) as u32;
-    let window_size = PhysicalSize::new(viewport_width, viewport_height);
-
-    let event_loop = EventLoop::new().unwrap();
-    let window = Rc::new(WindowBuilder::new()
-        .with_inner_size(window_size)
-        .with_resizable(false)
-        .with_title("Neural Network, No Cap")
-        .build(&event_loop)?);
-
-    let context = softbuffer::Context::new(window.clone()).unwrap();
-    let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
-    surface.resize(
-        NonZeroU32::new(viewport_width).unwrap(),
-        NonZeroU32::new(viewport_height).unwrap(),
-    )
-    .unwrap();
-
     let sim = Simulation::new(FIELD_WIDTH, FIELD_HEIGHT);
 
     let gray = Color::from(0x00_aa_aa_aa);
     let light_orange = Color::from(0x00_ff_ee_bf);
     let mut renderer = RendererBuilder::new()
-        .with_field_size(FIELD_WIDTH, FIELD_HEIGHT)
-        .with_block_size(BLOCK_SIZE)
         .with_field_color(light_orange)
         .with_border_color(gray)
-        .build().unwrap();
-
-    let mut now = Instant::now();
-    let mut delta_time = Duration::new(0,0);
-    let per_frame_duration = Duration::from_micros(MICROSECONDS_PER_FRAME);
-
-    event_loop.set_control_flow(ControlFlow::Poll);
-    event_loop.run(move |event, elwt| {
-        match event {
-            Event::WindowEvent { window_id, event: WindowEvent::RedrawRequested } if window_id == window.id() => {
-            }
-
-            Event::AboutToWait => {
-                let mut buffer = surface.buffer_mut().unwrap();
-                renderer.render(&mut buffer, &sim).unwrap();
-                buffer.present().unwrap();
-
-                delta_time = now.elapsed();
-                println!("{:?}", delta_time.as_micros());
-                now = Instant::now();
-            }
-
-            Event::WindowEvent { event: WindowEvent::KeyboardInput { event, ..}, window_id }
-                if window_id == window.id() => {
-                    let pressed_key;
-                    if let Some(key) = get_key_press(event) {
-                        pressed_key = key;
-                    }
-                    else { return }
-
-                    match pressed_key {
-                        NamedKey::Space => {
-                            println!("FPS: {:?}", 1_000_000 / delta_time.as_micros());
-                        },
-                        _ => ()
-                    }
-                }
-
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => {
-                elwt.exit();
-            }
-            _ => {}
-        }
-    })?;
+        .build()?;
 
     Ok(())
-}
-
-fn get_key_press(key_event: KeyEvent) -> Option<NamedKey> {
-    if !key_event.state.is_pressed() { return None }
-
-    if let Key::Named(named_key) = key_event.logical_key {
-        Some(named_key)
-    }
-    else { None }
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -38,19 +38,8 @@ struct RendererAttributes {
     pub width: usize,
     pub height: usize,
 
-    pub block_size: usize,
-    pub field_block_width: usize,
-    pub field_block_height: usize,
-
     pub field_color: Color,
     pub border_color: Color
-}
-
-impl RendererAttributes {
-    pub fn renew_total_size(&mut self) {
-        self.width = (self.block_size * self.field_block_width) + self.block_size;
-        self.height = (self.block_size * self.field_block_height) + self.block_size;
-    }
 }
 
 #[derive(Debug, Error)]
@@ -77,100 +66,6 @@ impl Renderer {
             is_initialized: false
         }
     }
-
-    fn init_render(&mut self, buffer: Buffer) -> Result<(), RendererError> {
-        let attr = &self.attr;
-
-        // Draw border
-        self.current_color = attr.border_color;
-
-        self.stamp_block(buffer, 0, 0, (attr.width, attr.block_size))?;
-        self.stamp_block(buffer, 0, attr.height - attr.block_size, (attr.width, attr.block_size))?;
-
-        self.stamp_block(buffer, 0, attr.block_size, (attr.block_size, attr.height - attr.block_size*2))?;
-        self.stamp_block(buffer, attr.width - attr.block_size, attr.block_size, (attr.block_size, attr.height - attr.block_size*2))?;
-
-        // Draw empty field
-        self.current_color = attr.field_color;
-
-        self.stamp_block(
-            buffer,
-            attr.block_size,
-            attr.block_size,
-            (attr.width - attr.block_size*2, attr.height - attr.block_size*2)
-        )?;
-
-        self.is_initialized = true;
-        Ok(())
-    }
-
-    pub fn render(&mut self, buffer: Buffer, sim: &Simulation) -> Result<(), RendererError> {
-        if !self.is_initialized {
-            self.init_render(buffer)?;
-        }
-
-        let mut pos: Vector2D<usize>;
-        for creature in sim.creatures() {
-            pos = creature.position();
-            self.current_color = creature.color();
-            self.grid_stamp_block(buffer, pos.x, pos.y)?;
-        }
-
-        Ok(())
-    }
-
-    // Stamp blocks of pixels according to the field grid
-    pub fn grid_stamp_block<T>(&self, buffer: Buffer, field_x: T, field_y: T) -> Result<(), RendererError>
-        where T: Into<usize> {
-        let field_x = field_x.into();
-        let field_y = field_y.into();
-
-        if field_x >= self.attr.field_block_width || field_y >= self.attr.field_block_height {
-            return Err(RendererError::OutOfFieldRange(field_x, field_y));
-        }
-
-        self.stamp_block(
-            buffer,
-            field_x * self.attr.block_size,
-            field_y * self.attr.block_size,
-            (self.attr.block_size, self.attr.block_size)
-        )?;
-
-        Ok(())
-    }
-
-    // Raw block stamping, without caring where it will go
-    // Starting from top left corner
-    fn stamp_block<T>(&self, buffer: Buffer, x: T, y: T, size: (T,T)) -> Result<(), RendererError>
-        where T: Into<usize> {
-        // Is this code ugly? Yes
-        let (stamp_width, stamp_height) = (size.0.into(), size.1.into());
-        let (x, y) = (x.into(), y.into());
-
-        for x_offset in 0..stamp_width.into() {
-            for y_offset in 0..stamp_height.into() {
-                self.plot_pixel(buffer, x + x_offset, y + y_offset, self.current_color)?;
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn plot_pixel<T>(&self, buffer: Buffer, x: T, y: T, color: Color) -> Result<(), RendererError>
-        where T: Into<usize> {
-
-        let (x, y) = (x.into(), y. into());
-
-        let parsed_index = x + (y*self.attr.width);
-
-        let slice_index = buffer
-            .get_mut(parsed_index)
-            .ok_or(RendererError::OutOfBufferRange(x, y))?;
-
-        *slice_index = color.rgb_u32();
-
-        Ok(())
-    }
 }
 
 pub struct RendererBuilder {
@@ -185,25 +80,11 @@ impl RendererBuilder {
     }
 
     pub fn build(mut self) -> Result<Renderer, RendererError> {
-        self.attr.renew_total_size();
         if self.attr.width == 0 || self.attr.height == 0 {
             return Err(RendererError::BufferTooSmall);
         }
 
         return Ok(Renderer::new(self.attr));
-    }
-
-    pub fn with_block_size(mut self, s: usize) -> Self {
-        self.attr.block_size = s;
-
-        self
-    }
-
-    pub fn with_field_size(mut self, w: usize, h: usize) -> Self {
-        self.attr.field_block_width = w;
-        self.attr.field_block_height = h;
-
-        self
     }
 
     pub fn with_field_color(mut self, color: Color) -> Self {
@@ -234,31 +115,5 @@ mod tests {
         let expected = 100_u32 | (234 << 8) | (88 << 16);
 
         assert_eq!(color.rgb_u32(), expected);
-    }
-
-    #[test]
-    fn recalculate_total_screensize() {
-        let mut attr = RendererAttributes {
-            width: 0,
-            height: 0,
-
-            block_size: 0,
-            field_block_width: 0,
-            field_block_height: 0,
-
-            field_color: Color::default(),
-            border_color: Color::default()
-        };
-
-        attr.block_size = 10;
-        attr.block_size = 10;
-
-        attr.field_block_width = 200;
-        attr.field_block_height = 200;
-
-        attr.renew_total_size();
-
-        assert_eq!(attr.width, 2000);
-        assert_eq!(attr.height, 2000);
     }
 }
