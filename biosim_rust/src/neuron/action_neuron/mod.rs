26f5fea5af3d88d58ef8522b44ee59758d68ab02
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-11 14:39:15 +0800

    Added simple Neuron traits and their Type eqiuvalent

--- biosim_rust/src/neuron/action_neuron/mod.rs
+++ biosim_rust/src/neuron/action_neuron/mod.rs
@@ -1,5 +1,12 @@
-pub trait ActionNeuron {
-    fn perform_action(&self) {
+use crate::simulation::Simulation;
+use crate::creature::Creature;
 
-    }
+
+pub trait ActionNeuron {
+    fn new() -> Self where Self: Sized;
+    fn perform_action(&self, sim: &mut Simulation, creature: &mut Creature);
+}
+
+pub trait ActionNeuronType {
+    const ID: u8;
 }
--- biosim_rust/src/neuron/sensory_neuron/mod.rs
+++ biosim_rust/src/neuron/sensory_neuron/mod.rs
@@ -1,5 +1,12 @@
-pub trait SensoryNeuron {
-    fn read_input(&self) {
+use crate::simulation::Simulation;
+use crate::creature::Creature;
 
-    }
+
+pub trait SensoryNeuron {
+    fn new() -> Self where Self: Sized;
+    fn read_input(&self, sim: &Simulation, creature: &Creature) -> f64;
+}
+
+pub trait SensoryNeuronType {
+    const ID: u8;
 }
