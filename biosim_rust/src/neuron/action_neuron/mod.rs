d1cbc921c00829ef84895e7a9c70b2233decd5a4
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-12 16:47:25 +0800

    Removed separate NeuronType under ActionNeuron and SensoryNeuron
    Opting for general NeuronType trait

--- biosim_rust/src/neuron/action_neuron/mod.rs
+++ biosim_rust/src/neuron/action_neuron/mod.rs
@@ -6,7 +6,3 @@ pub trait ActionNeuron {
     fn new() -> Self where Self: Sized;
     fn perform_action(&self, sim: &mut Simulation, creature: &mut Creature);
 }
-
-pub trait ActionNeuronType {
-    const ID: u8;
-}
--- biosim_rust/src/neuron/sensory_neuron/mod.rs
+++ biosim_rust/src/neuron/sensory_neuron/mod.rs
@@ -6,7 +6,3 @@ pub trait SensoryNeuron {
     fn new() -> Self where Self: Sized;
     fn read_input(&self, sim: &Simulation, creature: &Creature) -> f64;
 }
-
-pub trait SensoryNeuronType {
-    const ID: u8;
-}
