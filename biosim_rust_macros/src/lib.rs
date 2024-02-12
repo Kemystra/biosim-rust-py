63f2e1edecfc38fd2c0b76aae4132d1fddd67079
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-12 16:12:12 +0800

    Kinda completed neuron_type macro but not used lol

--- biosim_rust_macros/src/lib.rs
+++ biosim_rust_macros/src/lib.rs
@@ -1,19 +1,25 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, braced, parse_macro_input, token, Field, Ident, Token};
+use syn::{self, ItemStruct, parse_macro_input, token, LitInt, Ident, Token, parenthesized};
 use syn::parse::{Parse, ParseStream};
-use syn::punctuated::Punctuated;
-
-
 
 
 #[proc_macro_attribute]
 pub fn neuron_type(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let old_item = item.clone();
-    println!("{}", item.to_string());
+    let item_ast = parse_macro_input!(item as ItemStruct);
+    let neuron_id = parse_macro_input!(attr as LitInt);
 
-    old_item
+    let struct_name = &item_ast.ident;
+    if !struct_name.to_string().contains("Neuron") {
+        panic!("Expected `Neuron` in identifier");
+    }
+
+    let generated_impl = quote! {
+        #item_ast
+        impl NeuronType for #struct_name {
+            const ID: u8 = #neuron_id;
+        }
+    };
+
+    TokenStream::from(generated_impl)
 }
-
-
-
