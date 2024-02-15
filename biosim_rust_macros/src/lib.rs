df621ef321a069ee85e00d7725b6177d9b6d17e8
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-15 16:14:53 +0800

    Added new attribute macro to match ID from Genome to its corresponding neuron variants

--- biosim_rust_macros/src/lib.rs
+++ biosim_rust_macros/src/lib.rs
@@ -1,23 +1,28 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, ItemStruct, parse_macro_input, token, LitInt, Ident, Token, parenthesized};
-use syn::parse::{Parse, ParseStream};
+use syn::{self, ItemEnum, parse_macro_input};
 
 
 #[proc_macro_attribute]
-pub fn neuron_type(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item_ast = parse_macro_input!(item as ItemStruct);
-    let neuron_id = parse_macro_input!(attr as LitInt);
-
-    let struct_name = &item_ast.ident;
-    if !struct_name.to_string().contains("Neuron") {
-        panic!("Expected `Neuron` in identifier");
-    }
+pub fn enum_from_id(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let item_ast = parse_macro_input!(item as ItemEnum);
+    let enum_name = &item_ast.ident;
+    let match_arms = item_ast.variants
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| quote! { #i => Some(#enum_name::#x), });
 
     let generated_impl = quote! {
         #item_ast
-        impl NeuronType for #struct_name {
-            const ID: u8 = #neuron_id;
+
+        impl #enum_name {
+            pub fn from_id(id: usize) -> Option<Self> {
+                match id {
+                    #(#match_arms)*
+                    _ => None
+                }
+            }
         }
     };
 
