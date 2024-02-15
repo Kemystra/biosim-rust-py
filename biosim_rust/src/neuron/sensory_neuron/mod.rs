54d8ea6c97129666f336d94c757b89ecad0ac314
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-15 16:41:46 +0800

    Added enum_from_id attribute to SensoryNeuron and ActionNeuron enums

--- biosim_rust/src/neuron/sensory_neuron/mod.rs
+++ biosim_rust/src/neuron/sensory_neuron/mod.rs
@@ -1,7 +1,10 @@
+use biosim_rust_macros::enum_from_id;
+
 use crate::simulation::Simulation;
 use crate::creature::Creature;
 
 
+#[enum_from_id]
 pub enum SensoryNeuron {
     DistToBarrierNorth,
     DistToBarrierSouth,
