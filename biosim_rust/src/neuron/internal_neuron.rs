8af67e42494a1afd5e22d39673f069501b9b1c47
Kemystra <74447600+Kemystra@users.noreply.github.com>
2024-02-29 16:10:54 +0800

    Made InternalNeuron sets its state rather than returning its activation function value
    Also added getter for its state

--- biosim_rust/src/neuron/internal_neuron.rs
+++ biosim_rust/src/neuron/internal_neuron.rs
@@ -14,13 +14,16 @@ impl InternalNeuron {
     }
 
     // Uses Leaky ReLU
-    pub fn activation_func(&mut self, input: f64) -> f64 {
+    pub fn activation_func(&mut self, input: f64) -> () {
         let value = match input.total_cmp(&0_f64) {
             Ordering::Greater => input,
             _ => input * 0.01
         };
 
         self.state = value;
-        return value;
+    }
+
+    pub fn state(&self) -> f64 {
+        self.state
     }
 }
